dd79bd1405d42736792de6c778e9c51f29b9ae24
Ramon Damian James <damien.ramon@laposte.net>
2019-12-29 20:01:17 +0100

    Correction AssemblyInfo.cs

--- fastJSON/AssemblyInfo.cs
+++ fastJSON/AssemblyInfo.cs
@@ -8,17 +8,5 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © 2011-2019 Mehdi Gholam.")]
 
 
-[assembly: AssemblyVersion("2.2.5.0")]
-[assembly: AssemblyFileVersion("2.2.5.0")]
-=======
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("fastJSON")]
-[assembly: AssemblyDescription("smallest fastest polymorphic json serializer")]
-[assembly: AssemblyProduct("fastJSON")]
-[assembly: AssemblyCopyright("2010-2019")]
-
-
 [assembly: AssemblyVersion("2.3.0.0")]
 [assembly: AssemblyFileVersion("2.3.1.0")]
->>>>>>> pr/4
